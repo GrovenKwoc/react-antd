8d1c1f4eda12f94a3d851a61921132a249144b80
Groven Kwoc <GrovenKwoc@users.noreply.github.com>
2024-06-11 16:58:51 +0800

    发一版游戏，看看在vercel上能不能跑成功

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from "next";
+import { GameClientComponent} from "@/app/component/GameClientComponent";
 
 export const metadata: Metadata = {
   title: "记仇网|专业提供情绪价值",
 };
 
 export default function TestPage() {
-  return <div>test 01010</div>;
+  return <GameClientComponent/>;
 }
